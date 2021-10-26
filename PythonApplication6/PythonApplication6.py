b25f0f5f8b265dfad47705ddf9baee2e712e8fe3
opilane <opilane@HP-CZC6297B9M>
2021-10-26 11:52:23 +0300

    Имя. зарплата

--- PythonApplication6/PythonApplication6.py
+++ PythonApplication6/PythonApplication6.py
@@ -1,1 +1,27 @@
+from random import *
+inimesed=["A","B"]
+palgad=[1200,2000]
+
+def sisesta_andmed(i,p):
+    N=4
+    for n in range (N):
+        inimene=input("Nimi:")
+        i.append(inimene)
+        palk=randint(100,1000)
+        p.append(palk)
+    return i,p 
+def andmed_ekranile(i,p):
+    N=len(i)
+    for n in range(N):
+        print(i[n],"-",p[n])
+
+inimesed,palgad=sisesta_andmed(inimesed,palgad)
+andmed_ekranile(inimesed,palgad)
+
+
+
+
+
+
+
 
